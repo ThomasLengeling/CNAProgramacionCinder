269dc8eefe67bdb80b3913999765fdb18de6c361
Thomas Sanchez Lengeling <thomas@codigogenerativo.com>
2014-08-02 19:12:09 -0500

    basicapp

--- BasiApp/src/BasiAppApp.cpp
+++ BasiApp/src/BasiAppApp.cpp
@@ -8,27 +8,58 @@ using namespace std;
 class BasiAppApp : public AppNative {
   public:
 	void setup();
-	void mouseDown( MouseEvent event );	
+	void mouseDown( MouseEvent event );
+    void mouseDrag( MouseEvent event );
+    void mouseUp( MouseEvent event);
 	void update();
 	void draw();
+    
+    ci::Vec2f  mousePos;
+    ci::Vec2f  pMousePos;
+    bool       activateMouse;
 };
 
 void BasiAppApp::setup()
 {
+    gl::clear( Color( 0, 0, 0 ) );
+
 }
 
 void BasiAppApp::mouseDown( MouseEvent event )
 {
+    activateMouse = true;
+    pMousePos = mousePos;
+    mousePos = event.getPos();
 }
+
+void BasiAppApp::mouseDrag( MouseEvent event )
+{
+    if(activateMouse){
+        pMousePos = mousePos;
+        mousePos  = event.getPos();
+    }
+
+}
+
+void BasiAppApp::mouseUp(MouseEvent event)
+{
+    activateMouse = false;
+    pMousePos = mousePos;
+    mousePos  = event.getPos();
+}
+
 
 void BasiAppApp::update()
 {
+    
 }
 
 void BasiAppApp::draw()
 {
+    
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+//	gl::clear( Color( 0, 0, 0 ) );
+    gl::drawLine(mousePos, pMousePos);
 }
 
 CINDER_APP_NATIVE( BasiAppApp, RendererGl )
---
+++ BasiApp/xcode/build/BasiApp.build/Debug/BasiApp.build/Objects-normal/i386/BasiAppApp.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/tom/Documents/GitHub/CNAProgramacionCinder/BasiApp/src/BasiAppApp.cpp \
+  ../../../../Programming/Cinder/cinder_master/include/cinder/app/AppNative.h
