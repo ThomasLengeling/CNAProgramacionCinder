cbf776cf0ef202aa95db6e5c0ba1b8ac24f64e34
Thomas Sanchez Lengeling <thomas@codigogenerativo.com>
2014-08-07 17:34:30 -0500

    video

--- VideoTestQT/src/VideoTestQTApp.cpp
+++ VideoTestQT/src/VideoTestQTApp.cpp
@@ -1,5 +1,6 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include  "cinder/Rand.h"
 
 #include "cinder/Surface.h"
 #include "cinder/gl/Texture.h"
@@ -19,17 +20,39 @@ class VideoTestQTApp : public AppNative {
 	gl::Texture					mTexture;
 	ci::Surface                 mSurface;
 	ci::qtime::MovieGlRef		mMovie;
+
+	//Particula
+	ci::Vec2f * pos;
+	ci::Vec2f * vel;
+	float	   * radius;
+	ci::ColorA * col;
 };
 
 void VideoTestQTApp::setup()
 {
-	mMovie = qtime::MovieGl::create(loadAsset("video_test.mov"));
+	mMovie = qtime::MovieGl::create(loadAsset("video.mp4"));
 	mMovie->setLoop();
 	mMovie->play();
 
 	int  w = mMovie->getWidth();
 	int  h = mMovie->getHeight();
 	setWindowSize(w, h);
+
+	pos = new ci::Vec2f[100];
+	vel = new ci::Vec2f[100];
+	col = new ci::ColorA[100];
+	radius = new float[100];
+
+	for (int i = 0; i < 100; i++){
+		float x = ci::Rand::randFloat(200, 500);
+		float y = ci::Rand::randFloat(200, 500);
+		pos[i] = ci::Vec2f(x, y);
+		float vx = ci::Rand::randFloat(-0.1, 0.1);
+		float vy = ci::Rand::randFloat(-0.1, 0.1);
+		vel[i] = ci::Vec2f(vx, vy);
+		radius[i] = 2 + ci::Rand::randFloat(1, 6);
+	}
+	
 }
 
 void VideoTestQTApp::mouseDown( MouseEvent event )
@@ -40,17 +63,35 @@ void VideoTestQTApp::update()
 {
 	if (mMovie){
 		mTexture = mMovie->getTexture();
-		//mSurface = Surface(mMovie->getTexture());
+		if (mTexture){
+			int index = 0;
+			mSurface = Surface(mTexture);
+		}
+	}
+
+
+	if (mSurface){
+		for (int i = 0; i < 100; i++){
+			int x = (int)pos[i].x;
+			int y = (int)pos[i].y;
+			col[i] = mSurface.getPixel(ci::Vec2i(x, y));
+			pos[i] += vel[i];
+		}
 	}
 }
 
 void VideoTestQTApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) );
+	gl::clear( Color( 1, 1, 1 ) );
 
-	if (mTexture){
-		gl::draw(mTexture);
+	if (mSurface){
+		//gl::draw(mSurface);
+	}
+
+	for (int i = 0; i < 100; i++){
+		gl::color(col[i]);
+		gl::drawSolidCircle(pos[i], radius[i]);
 	}
 }
 
